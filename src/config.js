652afe3d6c61c5f77ed004dfa0f8416b055a4634
Nick Eberts <nicholas.eberts@yahoo.com>
2017-08-01 16:16:38 -0400

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "[URI for DocumentDB endpoint]";
-config.authKey = process.env.AUTH_KEY || "[Master key for DocumentDB]";
+config.host = process.env.HOST;
+config.authKey = process.env.AUTH_KEY;
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
